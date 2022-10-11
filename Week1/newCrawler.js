f4c7b8c23105bbc35c47af6ecb0b13f065ec2b10
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2022-10-12 00:09:06 +0700

    Add new crawler

---
+++ Week1/newCrawler.js
@@ -0,0 +1,81 @@
+const rp = require("request-promise");
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const URL = `https://jprp.vn/index.php/JPRP/issue/archive?fbclid=IwAR0wsHq3drGMoG8JsakKwAcvChfEvgNLUAKrN9YzN3-fxzXEk4_43JN0hYU`;
+
+var arr = [];
+
+async function getHref(link, htmlSelector) {
+    try {
+        // Lấy dữ liệu từ trang crawl đã được parseDOM
+        var $ = await rp(link);
+    } catch (error) {
+        return error;
+    }
+    var ds = $(htmlSelector);
+    var list = [];
+    ds.each(function(i, e) {
+        list.push({
+            url: e["attribs"]["href"],
+            transform: function(body) {
+                return cheerio.load(body);
+            }
+        })
+    });
+    return list;
+    
+};
+
+async function handle(link) {
+    var articleList = await getHref(link, "div.media-list div.article-summary div.media-body div.row div.col-md-10 a");
+    for(article of articleList) {
+        getContentArticle(article);
+    }
+
+}
+
+async function getContentArticle(link) {
+    try{
+        var $ = await rp(link);
+    } catch(error) {
+        console.log(error);
+        return error;
+    }
+    var articleName = $("article.article-details header h2").text().trim();
+    var author = $("#authorString > i").text().trim();
+    var date = $("body > div.pkp_structure_page > div.pkp_structure_content.container > main > div > article > div > section > div.list-group > div.list-group-item.date-published").text().trim();
+    var dateString = date.split('\t');
+    date = dateString[dateString.length - 1];
+    var numberArticleName = $("div.list-group div.issue div.panel-body a.title").text().trim();
+    var newObj = {
+        'articleName': articleName,
+        'author': author,
+        'date': date,
+        'numberArticleName': numberArticleName
+    }
+    console.log(newObj);
+    arr.push(newObj);
+}
+
+async function crawler() {
+    var optionList = await getHref({
+        url: URL,
+        transform: function(body) {
+            return cheerio.load(body);
+        }
+    }, "div.issue-summary div.media-body a.title");
+    var promise = new Promise(
+        function(resolve, reject) {
+            resolve();
+        }
+    )
+    for(var i = 0; i < optionList.length;) {
+        handle(optionList[i++]);
+    }
+    
+    fs.writeFileSync('data.JSON', JSON.stringify(arr));
+}
+
+crawler();
+
